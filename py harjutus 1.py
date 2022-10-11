26e2fee4ef6436f25d0616a9c8d22d735cccab8e
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2022-10-11 10:49:20 +0300

    Add files via upload

---
+++ py harjutus 1.py
@@ -0,0 +1,47 @@
+#Jan-Martti Olop
+#11.10.2022
+#harjutus 01
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print)'^^ "" ^^ ')
+
+
+
+
+
+a = int(input("Sisesta ruudu külje pikkus:"))
+s = a*a
+pirnt("Vastus: ruudu pindala on",s,"cm^2")
+
+
+
+
+
+#niisama harjutamine 
+s1 = int(input("lsia arv1: "))
+s2 = int(input ("lsia arv2: "))
+print(s1+s2)
+
+
+
+enimi = "mari"
+pnimi = "kisakõri"
+enimi = "kadri"
+nimi = enimi+" "+pnimi
+print(enimi,pnimi,"ja",nimi)
+vanus = 18 #int
+pikkus = 1.52 #float
+summa = vanus*pikkus # +-*/ //
+vastus = 5//2 #täisarvuline jagamine
+vastus = 5%2 #jääk
+print(vastus)
+
+
+
+
+print("tekst")
+print('tekst')
+print("it's my \"life\"")
