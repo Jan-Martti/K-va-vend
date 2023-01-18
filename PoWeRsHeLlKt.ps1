d903a3b0e847048a3d6b5299879aee4ff3a15b57
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-01-18 10:01:04 +0200

    Add files via upload

---
+++ PoWeRsHeLlKt.ps1
@@ -0,0 +1,24 @@
+﻿#Jan-Martti Olop
+#14.12.22
+#KT 4
+
+
+
+$inputFileName = Read-Host "Sisesta XML faili nimi"
+$rows = $inputFileName.customers.customer
+foreach ($row in $rows)
+{
+        
+        $nimi = $row.full_name
+        
+        $pask = "C:\temp\"+$nimi
+        $appi = $pask + $nimi + ".txt"
+        Get-Host C:\Users\it22\Desktop\ülesanne1.txt
+
+        New-Item -Path $pask -ItemType Directory
+        
+                
+       
+
+       
+}
